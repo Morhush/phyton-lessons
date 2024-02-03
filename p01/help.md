2cff5cc6809dba0056e93ee751c6956fdcfb4239
Morhush <sasha.morhush.z@gmail.com>
2024-02-03 14:36:04 +0200

    lesson1

---
+++ p01/help.md
@@ -0,0 +1,3 @@
+basic writing and formatting sy
+
+[md17](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#headings)
---
+++ p01/main.py
@@ -0,0 +1,9 @@
+# тут знаходиться пояснення
+
+print("Hello")
+print("Hello")
+#help(print)
+print('hello', 'my', 77, 'name', 'is', 'dmitro',567)
+print('tutorial','on','python','print','function')
+print('hello'+ 'my'+ 'name'+ 'is'+ 'denis')
+print('hello'+ 'my'+ '88' + 'is'+ 'denis')
