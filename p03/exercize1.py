9b8b74b9bf5b6b85b38cfda5e007d97af6813483
Morhush <sasha.morhush.z@gmail.com>
2024-02-17 14:33:02 +0200

    lesson2

---
+++ p03/exercize1.py
@@ -0,0 +1,50 @@
+# age = int(input('Enter your age:'))
+#
+# if age > 2:
+#     print('OK')
+# else:
+#     print('NO')
+
+# num = input('Input a number: ')
+#
+# result = num % 2
+# if result == 0:
+# if num % 2 == 0
+#
+#  if not num % 2:
+#     print(f'{num} is even')
+# else:
+#     print(f'{num} is odd')
+
+# num = int(input('Input a number: '))
+#
+# if num > 10 and num < 20:
+#     print('OK')
+# elif num > 90:
+#     print('> 90')
+# elif num > 50:
+#     print('> 50')
+# else:
+#         print('NO')
+
+# a = int(input('Input a number 1: '))
+# b = int(input('Input a number 2: '))
+# c = int(input('Input a number 3: '))
+#
+# if a >= b and a>=c:
+#     print(f'{a} is the largest')
+# elif b >=a and b >= c:
+#     print(f'{b} is the largest')
+# else:
+#     print(f'{c} is the largest')
+
+a = 10
+if a > 2:
+    if a < 30:
+        if a % 5 == 0:
+            print('OK')
+
+
+
+
+
