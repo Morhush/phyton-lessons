853737d1a1cb8287f33b8a530a3a1166ece5948b
Morhush <sasha.morhush.z@gmail.com>
2024-02-24 13:58:31 +0200

    lesson2

---
+++ p06/exercise1.py
@@ -0,0 +1,67 @@
+import random
+#list1 = [5, 7, 8, 10, 1]
+# list2 = ['Ivan', 'Dima', 'Rita']
+# list3 = ['Ivan', 2, 17, 'Dima', 'Rita', True, 3.14]
+# list4 = [list1, list2, list3]
+# print(list4)
+ # list1[0] = 999
+# print(list1)
+#
+# list_random_numbers = []
+# for i in range(1000):
+#     # random_number = random.randint(0, 10000)
+#     list_random_numbers.append(random.randint(0, 10000))
+#
+# print(list_random_numbers)
+#
+# # for i in range(100):
+# for item in list_random_numbers:
+#     print('Item = %d' % item)
+
+# for i in range(10):
+#     l = ['a', 'b', 'c', 'd', 'f']
+#     x = l [random.randint(0, 4)]
+#     print(x, end='')
+
+# nums = [1, 4, 6, 7, 4, 4, 7, 1, 2, 8]
+# print(nums)
+# count = 0
+# n = int(input('n: '))
+#
+# for xx in nums:
+#     if xx == n:
+#         # count = count + 1
+#         count += 1
+# print('Count %d in NUMS = %d' % (n,count))
+
+#
+# histogram = [12, 31, 6, 54, 9]
+# char = '\u2588'
+#
+# for h in histogram:
+#     # 1 h = 12
+#     # 2 h = 31
+#     print('')
+#     for n in range(h):
+#         print(char, end='')
+
+# number_list = [3, 4, 12, 7, 9, 8 ]
+# # x % 2 == 0
+# summa_even_number = 0
+# summa_odd_number = 0
+# for n in number_list:
+#     if n % 2 == 0:
+#         summa_even_number += n
+#     else:
+#         summa_odd_number += n
+#
+# print("Even number: ", summa_even_number)
+# print("Odd number: ", summa_odd_number)
+
+
+number = [3, 5, 12, 7, 8, 0, 22]
+
+print(number[::-1])
+
+
+
